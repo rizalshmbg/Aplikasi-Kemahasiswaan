f94564d1bf890e7df4dbe76df56fd0c8e8874c9f
rizalshmbg <sihombingrizal18@gmail.com>
2021-08-02 22:47:17 +0700

    membuat tampilan awal form mahasiswa

---
+++ src/kemahasiswaan_10119010_10119028/Kemahasiswaan_10119010_10119028.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kemahasiswaan_10119010_10119028;
+
+/**
+ *
+ * @author RizalSihombing
+ */
+public class Kemahasiswaan_10119010_10119028 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        frame_utama frame_utama = new frame_utama();
+        frame_utama.setVisible(true);
+    }
+    
+}
